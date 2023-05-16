aa1352780925fa52080e9cef5d2a4922c0a017b4
Aigars <aigarspeda@gmail.com>
2023-05-16 12:03:57 +0300

    work in progress google login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,12 +45,12 @@ const Home: NextPage = () => {
             Search
           </button>
 
-          {/* <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
-          </div> */}
+          </div>
         </div>
       </main>
     </>
